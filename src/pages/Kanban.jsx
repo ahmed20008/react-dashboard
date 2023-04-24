ef40c8642d6a4d19a62230a9250f72349a4da743
Ahmed <sss.ahm7@gmail.com>
2023-04-24 07:34:25 +0500

    kanban created

--- src/pages/Kanban.jsx
+++ src/pages/Kanban.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { KanbanComponent, ColumnsDirective, ColumnDirective } from '@syncfusion/ej2-react-kanban';
+import { kanbanData, kanbanGrid } from '../data/dummy';
+import { Header } from '../components';
 
 const Kanban = () => {
   return (
-    <div>Kanban</div>
+    <div className='m-2 md:md-10 mt-14 p-2 md:p-10 bg-white rounded-e-3xl'>
+      <Header category="App" title="Kanban" />
+      <KanbanComponent
+        id="kanban"
+        dataSource={kanbanData}
+        cardSettings={{contentField: 'Summary', headerField: 'Id'}}
+        keyField='Status'
+      >
+        <ColumnsDirective>
+          {kanbanGrid.map((items, index) =>
+            <ColumnDirective key={index} {...items} />
+          )}
+        </ColumnsDirective>
+      </KanbanComponent>
+    </div>
   )
 }
 
